b0f891ec60d4d30b52df2b82921abc6d2e479b5a
Kuraj_Boy <elcostes@ukr.net>
2020-07-26 18:12:28 +0300

    First commit

---
+++ script.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+echo "Hello it's first script"
+echo "Creating files..."
+	touch file1.txt
+	sh -c 'cat < EOF > file1.txt
+	UWU
+	Ebat Bodya Poluchilos
+	OWO'
+echo "Creating complete!"
+
+ver 1.0
